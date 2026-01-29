8de436284747e924063cc2bee20cdc5ad02e21bb
Le Chien <lhc021120@gmail.com>
2026-01-29 17:42:23 +0700

    fix direct

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return (
+    <div className="h-screen flex items-center justify-center flex-1 text-2xl font-semibold">
+      404 not found
+    </div>
+  );
+}
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,22 +1,23 @@
 import LogoBarLoading from "@/components/features/ui/LogoProgressLoading";
 import { lazy, Suspense } from "react";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 const MainLayout = lazy(() => import("@/layouts/MainLayout"));
-const PriceBoard = lazy(() => import("@/pages/PriceBoardPage"));
+const PriceBoardPage = lazy(() => import("@/pages/PriceBoardPage"));
+const NotFoundPage = lazy(() => import("@/pages/NotFoundPage"));
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Suspense fallback={<LogoBarLoading />}>
         <Routes>
-          {/* Root redirect */}
-          <Route path="/" element={<Navigate to="/price-board" replace />} />
-
           {/* Public layout */}
           <Route element={<MainLayout />}>
-            <Route path="/price-board" element={<PriceBoard />} />
+            <Route path="/" element={<PriceBoardPage />} />
           </Route>
+
+          {/* 404 */}
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
