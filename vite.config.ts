ed98b7e1f730327f069fbce6d81c2d9aabab0efb
Le Chien <lhc021120@gmail.com>
2026-06-11 16:00:44 +0700

    refactor build

--- vite.config.ts
+++ vite.config.ts
@@ -3,18 +3,32 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), tsconfigPaths()],
   build: {
+    chunkSizeWarningLimit: 600,
     rollupOptions: {
       output: {
-        manualChunks: {
-          "vendor-react": ["react", "react-dom", "react-router-dom"],
-          "vendor-mui": ["@mui/material"],
-          "vendor-firebase": ["firebase/app"],
-          "vendor-charts": ["lightweight-charts"],
-          "vendor-motion": ["framer-motion"],
+        manualChunks(id) {
+          if (
+            id.includes("ag-grid-community") ||
+            id.includes("ag-grid-react")
+          ) {
+            return "vendor-aggrid";
+          }
+          // React ecosystem
+          if (id.includes("react-dom") || id.includes("react-router-dom")) {
+            return "vendor-react";
+          }
+          if (id.includes("react-tooltip")) {
+            return "vendor-tooltip";
+          }
+          // Các vendor còn lại
+          if (id.includes("@mui/material")) return "vendor-mui";
+          if (id.includes("firebase")) return "vendor-firebase";
+          if (id.includes("lightweight-charts")) return "vendor-charts";
+          if (id.includes("framer-motion")) return "vendor-motion";
+          if (id.includes("react-joyride")) return "vendor-joyride";
         },
       },
     },
