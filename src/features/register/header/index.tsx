b5531dfce9f015b1050b9abdad860f204afa7728
Le Chien <lhc021120@gmail.com>
2026-07-08 13:30:27 +0700

    fix(ui): header register

--- src/features/register/header/index.tsx
+++ src/features/register/header/index.tsx
@@ -1,18 +1,20 @@
 import Logo from "@/assets/imgs/logo/lhc_logo.png";
 import ClientSetting from "@/components/features/header/component/ClientSetting";
-import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export default function RegisterHeader() {
-  const navigate = useNavigate();
-
   return (
-    <header className="flex items-center justify-between w-full h-full bg-bg-secondary md:px-2 pr-0.5 border-b border-border md:gap-8 gap-4 max-[425px]:gap-0">
-      <div
-        className="flex flex-row items-center md:gap-6 cursor-pointer"
-        onClick={() => navigate("/")}
-      >
-        <img src={Logo} alt="logo" className="w-12 h-full" />
-      </div>
+    <header className="flex items-center justify-between w-full h-full bg-bg-secondary md:px-4 px-2 pr-0.5 gap-1 md:gap-8">
+      <Link className="flex flex-row items-center gap-1 md:gap-2" to="/">
+        <img src={Logo} alt="logo" className="w-10 min-w-8 h-full" />
+
+        <div className="flex flex-row items-center gap-1 md:gap-2 max-[425px]:hidden">
+          <span className="text-base font-bold">WebTrade</span>
+          <div className="text-[8px] font-bold px-1 rounded bg-purple-selected text-white grid place-items-center h-max">
+            PRO
+          </div>
+        </div>
+      </Link>
 
       <ClientSetting />
     </header>
